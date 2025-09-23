557c6cf3cbfd3310f5a07fcfa3759c90fb0c2be1
Khanh_Poi <157507406+KhanhDesignWeb@users.noreply.github.com>
2025-09-23 11:00:47 +0700

    Xóa bớt file tạm

--- AccountAPI/Models/DrinkOrderContext.cs
+++ AccountAPI/Models/DrinkOrderContext.cs
@@ -17,10 +17,6 @@ public partial class DrinkOrderContext : DbContext
 
     public virtual DbSet<User> Users { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=Z14;Database=DrinkOrderDB;User ID=sa;Password=123456;TrustServerCertificate=True");
-
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<User>(entity =>
