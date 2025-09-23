ecba6831017d761daee76660992603ccaa2feec3
lploc75 <lpl070524@gmail.com>
2025-09-23 14:44:58 +0700

    view cho admin orders

---
+++ MVCApplication/DTOs/CreateOrderItemDto.cs
@@ -0,0 +1,9 @@
+﻿namespace MVCApplication.DTOs
+{
+    public class CreateOrderItemDto
+    {
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+    }
+}
---
+++ MVCApplication/DTOs/OrderDto.cs
@@ -0,0 +1,15 @@
+﻿namespace MVCApplication.DTOs
+{
+    public class OrderDto
+    {
+        public int OrderId { get; set; }
+        public string? OrderStatus { get; set; }
+        public string? PaymentStatus { get; set; }
+        public DateTime OrderDate { get; set; }
+        public decimal TotalAmount { get; set; }
+        public int UserId { get; set; }
+
+        // Danh sách items
+        public List<OrderItemDto>? Items { get; set; }
+    }
+}
