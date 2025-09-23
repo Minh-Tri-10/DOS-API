db2518f28d9d681bf321b5e38510bfdf011dbb2c
Khanh_Poi <157507406+KhanhDesignWeb@users.noreply.github.com>
2025-09-23 12:29:05 +0700

    Thêm api của category

---
+++ CategoriesAPI/Profile/CategoryProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using CategoriesAPI.DTOs;
+using CategoriesAPI.Models;
+
+namespace CategoriesAPI.Mapping;
+
+public class CategoryProfile : Profile
+{
+    public CategoryProfile()
+    {
+        CreateMap<Category, CategoryDTO>()
+            .ForMember(dest => dest.ProductIds, opt => opt.MapFrom(src => src.Products.Select(p => p.ProductId)));
+
+        CreateMap<CreateCategoryDTO, Category>();
+        CreateMap<UpdateCategoryDTO, Category>();
+    }
+}
---
+++ CategoriesAPI/Services/CategoryService.cs
@@ -0,0 +1,54 @@
+﻿using AutoMapper; // Thêm using này
+using CategoriesAPI.Repositories.Interfaces;
+using CategoriesAPI.Services.Interfaces;
+using CategoriesAPI.Models;
+using CategoriesAPI.DTOs;
+
+namespace CategoriesAPI.Services
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly ICategoryRepository _repository;
+        private readonly IMapper _mapper;
+
+        public CategoryService(ICategoryRepository repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        public async Task<IEnumerable<CategoryDTO>> GetAllAsync()
+        {
+            var categories = await _repository.GetAllAsync();
+            return _mapper.Map<IEnumerable<CategoryDTO>>(categories);
+        }
+
+        public async Task<CategoryDTO?> GetByIdAsync(int id)
+        {
+            var category = await _repository.GetByIdAsync(id);
+            return _mapper.Map<CategoryDTO>(category);
+        }
+
+        public async Task<CategoryDTO> AddAsync(CreateCategoryDTO dto) // Thay đổi return type thành CategoryDTO hoặc Category
+        {
+            var category = _mapper.Map<Category>(dto);
+            await _repository.AddAsync(category);
+            return _mapper.Map<CategoryDTO>(category); // Return DTO với Id mới
+        }
+
+        public async Task UpdateAsync(int id, UpdateCategoryDTO dto)
+        {
+            var category = await _repository.GetByIdAsync(id);
+            if (category != null)
+            {
+                _mapper.Map(dto, category); // Update fields
+                await _repository.UpdateAsync(category);
+            }
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            await _repository.DeleteAsync(id);
+        }
+    }
+}
