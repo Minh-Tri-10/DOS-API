287048fb04a897ace9155aedb389ead5d04d5faa
Khanh_Poi <157507406+KhanhDesignWeb@users.noreply.github.com>
2025-09-24 16:16:56 +0700

    gộp api product vào category

---
+++ CategoriesAPI/Models/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CategoriesAPI.Models;
+
+public partial class Product
+{
+    public int ProductId { get; set; }
+
+    public string ProductName { get; set; } = null!;
+
+    public string? Description { get; set; }
+
+    public decimal Price { get; set; }
+
+    public int? Stock { get; set; }
+
+    public int? CategoryId { get; set; }
+
+    public string? ImageUrl { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+
+    public DateTime? UpdatedAt { get; set; }
+
+    public virtual Category? Category { get; set; }  // Thêm dòng này để sửa lỗi
+
+}
